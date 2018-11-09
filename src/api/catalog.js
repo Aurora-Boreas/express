c646debfcf114097d43dbfce759fbb48ffa70242
Galina Borisova <g.borisova@kt-team.de>
2018-11-09 09:34:27 +0400

    task done

--- src/api/catalog.js
+++ src/api/catalog.js
@@ -2,47 +2,48 @@ import request from 'request';
 import ProcessorFactory from '../processor/factory';
 
 export default ({config, db}) => function (req, res, body) {
-	let groupId = null
 
-	// Request method handling: exit if not GET or POST
-	// Other metods - like PUT, DELETE etc. should be available only for authorized users or not available at all)
-	if (!(req.method == 'GET' || req.method == 'POST' || req.method == 'OPTIONS')) {
-		throw new Error('ERROR: ' + req.method + ' request method is not supported.')
-	}
+    if (!(req.method == 'GET' || req.method == 'POST' || req.method == 'OPTIONS')) {
+  	   res.status(404).send({ 'Method is not supported.' });
+  	}
 
-	const urlSegments = req.url.split('/');
+    const urlSegments = req.url.split('/');
 
-	let indexName = ''
-	let entityType = ''
-	if (urlSegments.length < 2)
-		throw new Error('No index name given in the URL. Please do use following URL format: /api/catalog/<index_name>/<entity_type>_search')
-	}
+    let indexName = ''
+  	let entityType = ''
 
-	// pass the request to elasticsearch
-	let url = 'http://' + config.elasticsearch.host + ':' + config.elasticsearch.port + req.url;
+    if (urlSegments.length = 2)
+      indexName = urlSegments[1];
 
-	request({ // do the elasticsearch request
-		uri: url,
-		method: req.method,
-		body: req.body,
-		json: true
-	}, function (_err, _res, _resBody) { // TODO: add caching layer to speed up SSR? How to invalidate products (checksum on the response BEFORE processing it)
-		if (_resBody && _resBody.hits && _resBody.hits.hits) { // we're signing up all objects returned to the client to be able to validate them when (for example order)
+  	if (urlSegments.length > 2)
+  		entityType = urlSegments[2]
 
-			const factory = new ProcessorFactory(config)
-			let resultProcessor = factory.getAdapter(entityType, indexName)
+  	if (config.elasticsearch.indices.indexOf(indexName) < 0) {
+  		throw new Error('Invalid / inaccessible index name given in the URL. Please do use following URL format: /api/catalog/<index_name>/_search')
+  	}
 
-			if (!resultProcessor)
-				resultProcessor = factory.getAdapter('default', indexName) // get the default processor
+  	if (urlSegments[urlSegments.length - 1].indexOf('_search') !== 0) {
+  		throw new Error('Please do use following URL format: /api/catalog/<index_name>/_search')
+  	}
 
-      if (entityType === 'product') {
-        resultProcessor.process(_resBody.hits.hits, groupId).then((result) => {
-          _resBody.hits.hits = result
-          res.json(_resBody);
-        }).catch((err) => {
-          console.error(err)
-        })
-      } else {
+    // pass the request to elasticsearch
+    //Elasticsearch's _search API expects only GET or POST
+  	let url = 'http://' + config.elasticsearch.host + ':' + config.elasticsearch.port + req.url;
+
+    request({ // do the elasticsearch request
+    uri: url,
+    method: req.method,
+    body: req.body,
+    json: true},
+     function (_err, _res, _resBody) { // TODO: add caching layer to speed up SSR? How to invalidate products (checksum on the response BEFORE processing it)
+    if (_resBody && _resBody.hits && _resBody.hits.hits) { // we're signing up all objects returned to the client to be able to validate them when (for example order)
+
+      const factory = new ProcessorFactory(config)
+      let resultProcessor = factory.getAdapter(entityType, indexName)
+
+      if (!resultProcessor)
+        resultProcessor = factory.getAdapter('default', indexName) // get the default processor
+
         resultProcessor.process(_resBody.hits.hits).then((result) => {
           _resBody.hits.hits = result
           res.json(_resBody);
@@ -51,8 +52,15 @@ export default ({config, db}) => function (req, res, body) {
         })
       }
 
-		} else {
-			res.json(_resBody);
-		}
-	});
+    } else {
+      res.json(_resBody);
+    }
+  });
+}
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', { error: err });
+})
+
 }
